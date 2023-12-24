7eb7dae173040757ba78fbdc46abe35441669894
shubhanshu0006 <shubhanshu.a.brudite@gmail.com>
2023-12-24 21:09:23 +0530

    django project with reverse mapping

--- newp/djanapp/djanapp/settings.py
+++ newp/djanapp/djanapp/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "djanapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        'ENGINE': 'django.db.backends.sqlite3',
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         'HOST': os.environ.get("DB_HOST"),
